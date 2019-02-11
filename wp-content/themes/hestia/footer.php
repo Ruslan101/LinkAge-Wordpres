db96278a2a0bc69a2e7cf6ee8df1e551c1ed17c4
Ruslan101 <ttracer64@gmail.com>
2019-02-11 05:07:04 +0300

    realise 1.0

--- wp-content/themes/hestia/footer.php
+++ wp-content/themes/hestia/footer.php
@@ -1,14 +1,11 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the "wrapper" div and all content after.
- *
- * @package Hestia
- * @since Hestia 1.0
- */
-?>
-			<?php do_action( 'hestia_do_footer' ); ?>
+
+			<div class="my_footer">
+				<a href="#"><img src="img/facebook.png" alt="facebook"></a>
+				<a href="#"><img src="img/twitter.png" alt="twitter"></a>
+				<a href="#"><img src="img/google.png" alt="google"></a>
+				<a href="#"><img src="img/linkedin.png" alt="linkedin"></a>
+				<a href="#"><img src="img/dribbble.png" alt="dribble"></a>
+			</div>
 		</div>
 	</div>
 <?php wp_footer(); ?>
